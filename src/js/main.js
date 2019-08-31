235f80bd1d3f2bf4c0152f176c9dde08b608ccf0
Mariusz Flis <mariusz.flis@gmail.com>
2019-08-31 14:43:02 +0200

    feat: migration homepage project to homepage-gulp

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,72 @@
-"use strict";
+/*alert('Witaj na mojej stronie');*/
+console.log('Witaj na mojej stronie');
+const name = 'Mariusz';
+const age = 39;
+/*console.log(name);
+console.log(age);*/
 
-// service worker registration - remove if you're not going to use it
+//skrypt powitalny Alert
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+/*const nameGuest = prompt('Podaj swoje imię:', 'wpisz imie');
 
-// place your code below
+if (nameGuest) {
+    ageGuest =prompt('Czy mozesz zdradzić ile masz lat:', 'wpisz wiek');
+    
+    if (ageGuest) 
+    { 
+        if (ageGuest==0) {alert ('Witaj!! ' + nameGuest +' albo jesteś niemowlakiem geniuszem, albo mnie okłamujesz');}
 
+        else
+        {
+        
+            if (ageGuest==age) 
+        
+            { alert('Witaj!! widzę ' + nameGuest +' ze jesteś w takim samym wieku co ja,cieszę sie ze odwiedza mnie rówieśnik');}
+            
+                else 
+                {
+ 
+                    if ((ageGuest>0)&&(ageGuest <= age)&&( calculateAge = ageGuest => { return age - ageGuest; } ))
+                    { alert ('Witaj!! ' + nameGuest +' jestes młodsza/y odemnie o :'+ calculateAge(ageGuest) +' rok/lat/lata, mam nadzieję ze znajdziesz tu coś ciekawego' );}  
 
-console.log(`Hello world!`);
+                        else
+                        {
 
+                            if ((ageGuest>0)&&(ageGuest >= age)&&( calculateAge = ageGuest => { return ageGuest - age; })) { alert ('Witaj!! '+ nameGuest +' jestes starsza/y odemnie o :'+calculateAge(ageGuest) +' rok/lat/lata, mam nadzieję ze znajdziesz tu coś ciekawego ');}
+                        
+                        }            
+            } 
+        }                                                  
+    } 
+    else { alert('Anulowałeś akcję szkoda, miałem dla Ciebie niespodziankę '); }
 
+} 
+
+else {
+    alert('Anulowałeś akcję szkoda, miałem dla Ciebie niespodziankę');
+}*/
+
+// Funkcja powitalna strzałkowa
+const welcome = (name1,age1) => console.log(`Witaj ${name1}, masz ${age1} lat!`); 
+const myName= 'Mariusz';
+const myAge= 39;
+
+welcome(myName,myAge);
+welcome('Mariusz', 39);
+
+const navigationSwitcher = document.querySelector('.navigation__switcher--js')
+
+navigationSwitcher.addEventListener('click', (e) => { 
+const navigationList = document.querySelector('.navigation__list--js');
+navigationList.classList.toggle('navigation__list--visible');
+});
+
+console.log(`Mam na imię ${name} mam ${age} lat, 🙈🙉🙊 to moja pierwsza strona WWW, i skrypt w JS 🛸`)
+
+const about = document.querySelector('.about__paragraph--js');
+console.log(about);
+console.log(about.innerHTML);
+
+about.innerHTML = `${about.innerHTML} : Mam na imię ${name} mam ${age} lat, 🙈🙉🙊 to moja pierwsza strona WWW, i skrypt w JS 🛸 <br> Początki z <b>D</b>ocument <b>O</b>bject <b>M</b>odel`;
+
+console.log(about.innerHTML);
